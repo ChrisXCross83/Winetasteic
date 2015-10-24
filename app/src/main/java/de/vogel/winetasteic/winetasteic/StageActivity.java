9941c4b6bae0bc2463a07ba7526e5d12d4fca702
Christian Braun <christian.braun@vogel.de>
2015-10-24 02:45:53 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	app/src/main/res/layout/activity_challenge.xml

--- app/src/main/java/de/vogel/winetasteic/winetasteic/StageActivity.java
+++ app/src/main/java/de/vogel/winetasteic/winetasteic/StageActivity.java
@@ -26,8 +26,8 @@ public class StageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stage);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         id = getIntent().getIntExtra("index",0);
 
